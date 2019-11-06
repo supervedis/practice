bc94165a2bb738a934e166f50158d1bacaeb7928
dlumbasi <vedisn@gmail.com>
2019-11-06 23:01:38 +1100

    Added ID to models

---
+++ Java/Spring/pet-clinic/pet-clinic-data/src/main/java/lumbah/springframework/petclinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package lumbah.springframework.petclinic.model;
+
+public class BaseEntity {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
