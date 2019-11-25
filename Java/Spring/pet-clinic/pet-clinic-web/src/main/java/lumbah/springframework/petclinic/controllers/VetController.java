728c811a71acd8d6090f79e8e6914929afc45bfb
dlumbasi <vedisn@gmail.com>
2019-11-26 06:29:18 +1100

    List all Vets on Vet index page. Closes #15

--- Java/Spring/pet-clinic/pet-clinic-web/src/main/java/lumbah/springframework/petclinic/controllers/VetController.java
+++ Java/Spring/pet-clinic/pet-clinic-web/src/main/java/lumbah/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package lumbah.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lumbah.springframework.petclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
 	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
