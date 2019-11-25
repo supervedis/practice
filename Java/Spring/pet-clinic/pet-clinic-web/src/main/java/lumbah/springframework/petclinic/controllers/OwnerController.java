0ee16b11c804a70d3a3539d8df59809545e5628f
dlumbasi <vedisn@gmail.com>
2019-11-25 15:26:52 +1100

    Create owner index and page controller. Closes #13

---
+++ Java/Spring/pet-clinic/pet-clinic-web/src/main/java/lumbah/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package lumbah.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/owner"})
+@Controller
+public class OwnerController {
+
+	@RequestMapping({"","/","/index","/index.html"})
+	public String listOwner() {
+		return "owner/index";
+	}
+}
