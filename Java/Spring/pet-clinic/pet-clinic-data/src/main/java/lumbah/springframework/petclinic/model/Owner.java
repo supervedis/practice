0b21bbd431d0ec325c49c446709361c8245c74ba
dlumbasi <vedisn@gmail.com>
2019-11-26 06:00:09 +1100

    List all Owners on Owners Index page

--- Java/Spring/pet-clinic/pet-clinic-data/src/main/java/lumbah/springframework/petclinic/model/Owner.java
+++ Java/Spring/pet-clinic/pet-clinic-data/src/main/java/lumbah/springframework/petclinic/model/Owner.java
@@ -1,20 +1,4 @@
 package lumbah.springframework.petclinic.model;
 
-public class Owner extends BaseEntity{
-	private String firstName;
-	private String lastName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
+public class Owner extends Person{
 }
--- Java/Spring/pet-clinic/pet-clinic-web/src/main/java/lumbah/springframework/petclinic/controllers/OwnerController.java
+++ Java/Spring/pet-clinic/pet-clinic-web/src/main/java/lumbah/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package lumbah.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lumbah.springframework.petclinic.service.OwnerService;
 
 @RequestMapping({"/owner"})
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String listOwner() {
+	public String listOwner(Model model) {
+		model.addAttribute("owners", ownerService.findAll());
 		return "owner/index";
 	}
 }
