2d116cc55cee4c5eeeb89cb4037e5062661a104e
supervedis <vedisn@gmail.com>
2021-09-14 01:07:13 +0300

    Added Dollar and DollarMultiplication classes

---
+++ Java/TDD-By-Example/src/com/wycash/Dollar.java
@@ -0,0 +1,13 @@
+package com.wycash;
+
+public class Dollar {
+
+	public int amount;
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	
+	public void times(int multiplier) {
+		amount = amount * multiplier;
+	}
+}
