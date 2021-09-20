8048c3a0086d1bee84d8532eb1e1978a897fb5d2
supervedis <vedisn@gmail.com>
2021-09-20 14:03:23 +0300

    Created Super Money class and moved amount and equals method from Dollar and Franc to Money class

--- Java/TDD-By-Example/src/com/wycash/Dollar.java
+++ Java/TDD-By-Example/src/com/wycash/Dollar.java
@@ -1,19 +1,12 @@
 package com.wycash;
 
-public class Dollar {
+public class Dollar extends Money{
 
-	private int amount;
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
 	
 	public Dollar times(int multiplier) {
 		return new Dollar(amount * multiplier);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		Dollar dollar = (Dollar)obj;
-		return amount==dollar.amount;
 	}
 }
