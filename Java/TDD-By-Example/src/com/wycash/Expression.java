0c3ce00a92b3ad8cea64001c5be6b74d9ef9977f
supervedis <vedisn@gmail.com>
2021-09-30 15:10:49 +0300

    Added Expression Interface and Bank class to aid in cross currency operations.

---
+++ Java/TDD-By-Example/src/com/wycash/Expression.java
@@ -0,0 +1,5 @@
+package com.wycash;
+
+public interface Expression {
+
+}
--- Java/TDD-By-Example/src/com/wycash/Money.java
+++ Java/TDD-By-Example/src/com/wycash/Money.java
@@ -1,6 +1,6 @@
 package com.wycash;
 
-public class Money {
+public class Money implements Expression{
 	protected int amount;
 	protected String currency;
 	
@@ -19,6 +19,10 @@ public class Money {
 	
 	Money times(int multiplier) {
 		return new Money(amount*multiplier, currency);
+	}
+	
+	Expression plus(Money addend) {
+		return new Money(amount + addend.amount, currency);
 	}
 	
 	String currency() {
