bf9c2fab96f68834ac8e175df275866d3de0d432
supervedis <vedisn@gmail.com>
2021-09-30 13:48:21 +0300

    Refactored the Money factory methods.

--- Java/TDD-By-Example/src/com/wycash/DollarMultiplicationTest.java
+++ Java/TDD-By-Example/src/com/wycash/DollarMultiplicationTest.java
@@ -17,16 +17,7 @@ class DollarMultiplicationTest {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-	}
-	
-	@Test
-	void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
 	}
 	
 	@Test
@@ -35,8 +26,4 @@ class DollarMultiplicationTest {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 	
-	@Test
-	void testDifferentClassEquality() {
-		assertTrue(new Money(10,"CHF").equals(new Franc(10, "CHF")));
-	}
 }
